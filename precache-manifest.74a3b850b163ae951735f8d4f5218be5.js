019970fcc746657a6d042c2b0c0500ba1130abe9
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2018-12-18 07:46:43 +0530

    Updates

---
+++ precache-manifest.74a3b850b163ae951735f8d4f5218be5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb0a9a0aef783860160c",
+    "url": "/blue-macaw-client/static/js/runtime~main.cb0a9a0a.js"
+  },
+  {
+    "revision": "d1bd5c57495117577385",
+    "url": "/blue-macaw-client/static/js/main.d1bd5c57.chunk.js"
+  },
+  {
+    "revision": "33738cecdcba41d76ea3",
+    "url": "/blue-macaw-client/static/js/4.33738cec.chunk.js"
+  },
+  {
+    "revision": "3abe55dc59170984d46c",
+    "url": "/blue-macaw-client/static/js/3.3abe55dc.chunk.js"
+  },
+  {
+    "revision": "c78b876aa0985768fcc5",
+    "url": "/blue-macaw-client/static/js/2.c78b876a.chunk.js"
+  },
+  {
+    "revision": "9e1ad01aef6062467890",
+    "url": "/blue-macaw-client/static/js/1.9e1ad01a.chunk.js"
+  },
+  {
+    "revision": "d1bd5c57495117577385",
+    "url": "/blue-macaw-client/static/css/main.beeb5bb2.chunk.css"
+  },
+  {
+    "revision": "c3ff35af4b98dcf14fc2cd6cc18b03ab",
+    "url": "/blue-macaw-client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a033923fc5c171c3f16591211eaa2b6.js"
+  "/blue-macaw-client/precache-manifest.74a3b850b163ae951735f8d4f5218be5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/blue-macaw-client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
